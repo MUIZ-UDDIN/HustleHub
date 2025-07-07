76359c6465197ff85a245750ae4961f0d84246f1
Muiz Ud Din <mueezgamings@gmail.com>
2025-07-08 02:08:19 +0500

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-  base: '/Hustle-Hub/', // <-- Add this line (use your repo name)
-})
+  plugins: [tailwindcss(), react()],
+  base: "/Hustle-Hub/", // 👈 this MUST match your repo name
+});
