3b848ea983219f7c4108b6e4a87056d10f4eeafe
Muiz Ud Din <mueezgamings@gmail.com>
2025-07-08 03:11:51 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/Hustle-Hub/", // 👈 this MUST match your repo name
+  base: process.env.NODE_ENV === "production" ? "/HustleHub" : "/",
 });
