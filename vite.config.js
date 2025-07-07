5d48ce47f96cc83642d5618d566284b5ec7b23cc
MUIZ-UDDIN <86313423+MUIZ-UDDIN@users.noreply.github.com>
2025-07-08 01:58:38 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  base: '/Hustle-Hub/', // <-- Add this line (use your repo name)
 })
