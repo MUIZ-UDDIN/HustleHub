da1645472c318381cd0572116803f2eb59e6f326
Muiz Ud Din <mueezgamings@gmail.com>
2025-07-08 17:41:39 +0500

    Fix: Added ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop"; // Adjust path if needed
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -6,13 +7,14 @@ import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-      <main className="fade-in">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
+    <main className="fade-in">
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </main>
   );
 }
